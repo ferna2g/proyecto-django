9c81b65b4911ac3191ff7603408b81e4891a00be
fer g <arielgeronimo517@gmail.com>
2020-03-25 20:19:26 -0600

    version 1

---
+++ tienda_online/tienda_online/views.py
@@ -0,0 +1,70 @@
+from django.shortcuts import render
+from django.shortcuts import redirect
+#from django.http import HttpResponse
+
+from django.contrib import messages
+from django.contrib.auth import login
+from django.contrib.auth import logout 
+from django.contrib.auth import authenticate
+
+from users.models import User
+from .forms import RegisterForm
+
+from products.models import Product
+
+def index(request):
+
+	products = Product.objects.all().order_by('-id')
+
+	return render(request, 'index.html', {
+    	    'message': 'Lista de productos',
+        	'title': 'Productos',
+        	'products': products,
+    	})
+
+
+def login_view(request):
+	if request.user.is_authenticated:
+		return redirect('index')
+		
+	if request.method == 'POST':
+		username = request.POST.get('username') #diccionario
+		password = request.POST.get('password') #None
+
+		user = authenticate(username=username, password=password) #None
+		if user:
+			login(request, user)
+			messages.success(request, 'Bienvenido {}'.format(user.username))
+			return redirect('index')
+		else:
+			messages.error(request, 'Usuario o contrasena no validos')
+
+	return render(request, 'users/login.html',{
+
+		})
+
+
+def logout_view(request):
+	logout(request)
+	messages.success(request, 'Sesion cerrada exitosamente')
+	return redirect('login')
+
+
+def register(request):
+	if request.user.is_authenticated:
+		return redirect('index')
+
+	form = RegisterForm(request.POST or None)
+
+	if request.method == 'POST' and form.is_valid():
+
+		user = form.save()
+		
+		if user:
+			login(request, user)
+			messages.success(request, 'Usuario creado exitosamente')
+			return redirect('index')
+
+	return render(request, 'users/register.html', {
+			'form': form
+		})
